79c9c2689ff7913b7eac52bcd16f233335800fec
KADDOURI <ismail.kaddouri@capgemini.com>
2022-01-13 12:44:40 +0100

    first commit

--- src/main/java/com/hamilton/proxibanque/ProxiBanqueApplication.java
+++ src/main/java/com/hamilton/proxibanque/ProxiBanqueApplication.java
@@ -1,4 +1,4 @@
-package com.example.proxibanque;
+package com.hamilton.proxibanque;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
