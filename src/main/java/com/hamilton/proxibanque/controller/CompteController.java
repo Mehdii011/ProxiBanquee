1abea15165302e5da96393bcb6160e84407a3a25
ikaddour <ismail.kaddouri@capgemini.com>
2022-01-20 21:32:28 +0100

    Gestion compte et client(modified)

--- src/main/java/com/hamilton/proxibanque/controller/CompteController.java
+++ src/main/java/com/hamilton/proxibanque/controller/CompteController.java
@@ -11,6 +11,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.http.HttpClient;
 import java.util.List;
 import java.util.Optional;
 
@@ -21,7 +22,7 @@ public class CompteController {
     private IBanqueService iBanqueService;
 
     @GetMapping("/listcomptes")
-    public ResponseEntity<List<Compte>> listeCompte(@RequestParam(value = "page", defaultValue = "1") int page, @RequestParam(value = "limit",defaultValue = "5") int limit) {
+    public ResponseEntity<List<Compte>> listeCompte(@RequestParam(value = "page", defaultValue = "1") int page, @RequestParam(value = "limit", defaultValue = "5") int limit) {
         List<Compte> comptes = iBanqueService.listeCompte(page, limit);
         return new ResponseEntity<>(comptes, HttpStatus.OK);
     }
@@ -31,22 +32,42 @@ public class CompteController {
         Optional<Compte> compte = iBanqueService.consulterCompte(compteId);
         return ResponseEntity.ok().body(compte.isPresent() ? compte.get() : null);
     }
+
     @GetMapping("/listoperations/{numCompte}")
-    public ResponseEntity<List<Operation>> listeOperation(@PathVariable(value = "numCompte") Long compteId,@RequestParam(value = "page", defaultValue = "1") int page, @RequestParam(value = "limit",defaultValue = "5") int limit) {
-        List<Operation> operations = iBanqueService.operations(compteId,page, limit);
+    public ResponseEntity<List<Operation>> listeOperation(@PathVariable(value = "numCompte") Long compteId, @RequestParam(value = "page", defaultValue = "1") int page, @RequestParam(value = "limit", defaultValue = "5") int limit) {
+        List<Operation> operations = iBanqueService.operations(compteId, page, limit);
         return new ResponseEntity<>(operations, HttpStatus.OK);
     }
-    @PostMapping("/saveOperation")
-    public ResponseEntity<Operation> saveOperation(@RequestBody Operation operation,@RequestParam(value = "typeOperation") String typeOperation,Long numCompte2) throws CompteIntrouvable, DebitImpossibleException {
-        if(typeOperation.equals("Versement")){
-            iBanqueService.crediter(operation.getCompte().getNumeroCompte(),operation.getMontant());}
-        else if(typeOperation.equals("Retrait")){
-            iBanqueService.debiter(operation.getCompte().getNumeroCompte(),operation.getMontant());
-        }
-        if(typeOperation.equals("Virement")){
-            iBanqueService.virement(operation.getCompte().getNumeroCompte(),numCompte2,operation.getMontant());
-        }
+
+    @PostMapping("/saveVersement")
+    public ResponseEntity<Operation> saveVersement(@RequestBody Operation operation) throws CompteIntrouvable {
+        iBanqueService.crediter(operation.getCompte().getNumeroCompte(), operation.getMontant());
+
         return ResponseEntity.ok(operation);
     }
+
+    @PostMapping("/saveRetrait")
+    public ResponseEntity<Operation> saveOperation(@RequestBody Operation operation) throws CompteIntrouvable, DebitImpossibleException {
+        iBanqueService.debiter(operation.getCompte().getNumeroCompte(), operation.getMontant());
+        return ResponseEntity.ok(operation);
     }
+
+    @PostMapping("/saveVirement")
+    public ResponseEntity<Operation> saveOperation(@RequestBody Operation operation, @RequestBody Long numCompte2) throws CompteIntrouvable, DebitImpossibleException {
+        try {
+            Optional<Compte> compte = iBanqueService.consulterCompte(numCompte2);
+            if (compte.isEmpty()) {
+                throw new CompteIntrouvable("Transaction Impossible : Compte bénéficiaire n'existe pas");
+            }
+            iBanqueService.virement(operation.getCompte().getNumeroCompte(), numCompte2, operation.getMontant());
+
+            return ResponseEntity.ok(operation);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+
+        }
+
+    }
+
+}
 
