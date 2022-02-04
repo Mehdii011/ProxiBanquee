d2faca6aa351f2f7b249843b41d646b710c8e51a
JABIR <fatima-ezahra.jabir@capgemini.com>
2022-02-04 12:09:23 +0100

    service_compte

---
+++ src/main/java/com/hamilton/proxibanque/services/CompteServiceImpl.java
@@ -0,0 +1,48 @@
+package com.hamilton.proxibanque.services;
+
+import com.hamilton.proxibanque.dao.CompteRepository;
+import com.hamilton.proxibanque.model.Compte;
+import com.hamilton.proxibanque.model.CompteCourant;
+import com.hamilton.proxibanque.model.CompteEpargne;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+@Transactional
+@Slf4j
+public class CompteServiceImpl implements ICompteService{
+
+    @Autowired
+    private CompteRepository compteRepository;
+
+
+    @Override
+    public Compte createcompteCourant(CompteCourant compte) {
+      return  compteRepository.save(compte);
+        //log.info("compte saved  with success {}", compte.getNumeroCompte());
+    }
+    @Override
+    public Compte createcompteEpargne(CompteEpargne compte) {
+       return compteRepository.save(compte);
+      //  log.info("compte saved  with success {}", compte.getNumeroCompte());
+    }
+
+    @Override
+    public void editcompte(Compte compte) {
+        compteRepository.save(compte);
+        log.info("compte saved  with success {}", compte.getNumeroCompte());
+    }
+
+
+
+    @Override
+    public void destroycompte(Long numCompte) {
+        compteRepository.deleteById(numCompte);
+        log.info("compte deleted with success {}", numCompte);
+    }
+
+
+}
---
+++ src/main/java/com/hamilton/proxibanque/services/ICompteService.java
@@ -0,0 +1,31 @@
+package com.hamilton.proxibanque.services;
+
+import com.hamilton.proxibanque.model.Compte;
+import com.hamilton.proxibanque.model.CompteCourant;
+import com.hamilton.proxibanque.model.CompteEpargne;
+
+import javax.transaction.Transactional;
+
+@Transactional
+public interface ICompteService {
+
+    Compte createcompteCourant(CompteCourant compte) ;
+    Compte createcompteEpargne(CompteEpargne compte) ;
+
+
+
+
+
+
+
+
+
+
+    void editcompte(Compte compte) ;
+
+    void destroycompte(Long numCompte) ;
+
+
+
+
+}
