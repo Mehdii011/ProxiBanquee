ffccd08cddfa61fb97001ae9a6dd10286245fe8d
JABIR <fatima-ezahra.jabir@capgemini.com>
2022-02-04 02:11:44 +0100

    gestion compte

--- src/main/java/com/hamilton/proxibanque/dao/ClientRepository.java
+++ src/main/java/com/hamilton/proxibanque/dao/ClientRepository.java
@@ -4,7 +4,9 @@ import com.hamilton.proxibanque.model.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface ClientRepository extends JpaRepository<Client,Long> {
+    public List<Client> findAll();
 }
--- src/main/java/com/hamilton/proxibanque/dao/CompteRepository.java
+++ src/main/java/com/hamilton/proxibanque/dao/CompteRepository.java
@@ -3,9 +3,9 @@ package com.hamilton.proxibanque.dao;
 import com.hamilton.proxibanque.model.Compte;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
-
+import java.util.List;
 
 @Repository
 public interface CompteRepository extends PagingAndSortingRepository<Compte,Long> {
-
+ public List<Compte> findAll();
 }
